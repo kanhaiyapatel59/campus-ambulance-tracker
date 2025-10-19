bc54923f51f2bb5b1c8a264c4c2f6a9be347855c
kanhaiyapatel59 <kanhaiyapatel.2024cse@sece.ac.in>
2025-10-19 16:18:23 +0530

    Campus Ambulance Tracker Initialized

---
+++ src/main/java/com/campus/safety/ambulancetracker/AmbulanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.campus.safety.ambulancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbulanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbulanceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campus/safety/ambulancetracker/AmbulanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.safety.ambulancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbulanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
